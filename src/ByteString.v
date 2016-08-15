ad1d15573995abcabd2e6206fd22ac3c516376f6
John Wiegley <johnw@newartisans.com>
2016-08-15 14:44:32 -0700

    Remove unnecessary code from ByteString.v

--- src/ByteString.v
+++ src/ByteString.v
@@ -1,39 +1,26 @@
 Require Import
   Fiat.ADT
   Fiat.ADTNotation
-  ByteString.ADTInduction.
+  ByteString.Heap.
 
-Require Import
-  Coq.Lists.List
-  Coq.NArith.NArith.
-
-Section ByteString.
+Module ByteString (Mem : Memory).
 
 (************************************************************************
  ** Semantics of the core Haskell ByteString data type in Fiat.        **
  ************************************************************************)
-
-Variable Word8 : Type.
-
-Open Scope string_scope.
 
 Definition emptyS  := "empty".
 Definition consS   := "cons".
 Definition unconsS := "uncons".
 
 Definition ByteStringSpec := Def ADT {
-  rep := list Word8,
+  rep := list Mem.Word8,
 
-  (* Introduction *)
+  Def Constructor0 emptyS : rep := ret []%list,,
 
-  Def Constructor0 emptyS : rep := ret [],,
-
-  Def Method1 consS (r : rep) (w : Word8) : rep * unit :=
+  Def Method1 consS (r : rep) (w : Mem.Word8) : rep * unit :=
     ret (cons w r, tt),
-
-  (* Elimination *)
-
-  Def Method0 unconsS (r : rep) : rep * (option Word8) :=
+  Def Method0 unconsS (r : rep) : rep * (option Mem.Word8) :=
     match r with
     | nil => ret (r, None)
     | cons x xs => ret (xs, Some x)
@@ -46,10 +33,10 @@ Definition ByteString := Rep ByteStringSpec.
 Definition empty : Comp ByteString :=
   Eval simpl in callCons ByteStringSpec emptyS.
 
-Definition cons (w : Word8) (bs : ByteString) : Comp ByteString :=
+Definition cons (w : Mem.Word8) (bs : ByteString) : Comp ByteString :=
   Eval simpl in (p <- callMeth ByteStringSpec consS bs w; ret (fst p)).
 
-Definition uncons (bs : ByteString) : Comp (ByteString * option Word8) :=
+Definition uncons (bs : ByteString) : Comp (ByteString * option Mem.Word8) :=
   Eval simpl in callMeth ByteStringSpec unconsS bs.
 
 End ByteString.
