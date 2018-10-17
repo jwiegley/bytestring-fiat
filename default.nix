5e778650fbdbc787fa4ab310a8557630927c858e
John Wiegley <johnw@newartisans.com>
2018-10-16 17:33:39 -0700

    Remove Fiat and coq-haskell from _CoqProject; they should be installed

---
+++ default.nix
@@ -0,0 +1,39 @@
+{ packages ? "coqPackages_8_7"
+
+, rev      ? "d1ae60cbad7a49874310de91cd17708b042400c8"
+, sha256   ? "0a1w4702jlycg2ab87m7n8frjjngf0cis40lyxm3vdwn7p4fxikz"
+
+, pkgs     ? import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    inherit sha256; }) {
+    config.allowUnfree = true;
+    config.allowBroken = false;
+  }
+}:
+
+with pkgs.${packages}; pkgs.stdenv.mkDerivation rec {
+  name = "bytestring";
+  version = "1.0";
+
+  src =
+    if pkgs.lib.inNixShell
+    then null
+    else if pkgs ? coqFilterSource
+         then pkgs.coqFilterSource [] ./.
+         else ./.;
+
+  buildInputs = [
+    coq coq.ocaml coq.camlp5 coq.findlib
+    fiat_HEAD coq-haskell
+  ];
+  enableParallelBuilding = true;
+
+  buildPhase = "make -j$NIX_BUILD_CORES";
+  preBuild = "coq_makefile -f _CoqProject -o Makefile";
+  installFlags = "COQLIB=$(out)/lib/coq/${coq.coq-version}/";
+
+  env = pkgs.buildEnv { name = name; paths = buildInputs; };
+  passthru = {
+    compatibleCoqVersions = v: builtins.elem v [ "8.6" "8.7" ];
+ };
+}
